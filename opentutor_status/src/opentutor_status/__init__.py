7dd2127645e4795896b0cf9823de80c48026f8a0
larry kirschner <larrykirschner@gmail.com>
2021-06-21 17:10:58 -0700

    fixes tests

--- opentutor_status/src/opentutor_status/__init__.py
+++ opentutor_status/src/opentutor_status/__init__.py
@@ -1,5 +1,5 @@
 #
-# This software is Copyright ©️ 2020 The University of Southern California. All Rights Reserved. 
+# This software is Copyright ©️ 2020 The University of Southern California. All Rights Reserved.
 # Permission to use, copy, modify, and distribute this software and its documentation for educational, research and non-profit purposes, without fee, and without a written agreement is hereby granted, provided that the above copyright notice and subject to the full license file found in the root of this software deliverable. Permission to make commercial use of this software may be obtained by contacting:  USC Stevens Center for Innovation University of Southern California 1150 S. Olive Street, Suite 2300, Los Angeles, CA 90115, USA Email: accounting@stevens.usc.edu
 #
 # The full terms of this copyright and license should always be found in the root directory of this software deliverable as "license.txt" and if these terms are not found with this software, please contact the USC Stevens Center for the full license.
@@ -8,8 +8,6 @@ from logging.config import dictConfig
 
 from flask import Flask
 from flask_cors import CORS
-
-# from .config_default import Config
 
 
 def create_app():
@@ -32,11 +30,10 @@ def create_app():
         }
     )
     app = Flask(__name__)
-    # app.config.from_object(Config)
     CORS(app)
 
-    from opentutor_status.blueprints.healthcheck import healthcheck_blueprint
+    from .blueprints.status import status_blueprint
 
-    app.register_blueprint(healthcheck_blueprint, url_prefix="/classifier/healthcheck")
+    app.register_blueprint(status_blueprint, url_prefix="/status")
 
     return app
